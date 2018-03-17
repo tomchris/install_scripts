0c1df25c7011a62ae5d2f00d98cce5365bc2a78e
tomchris <tom@dyvantage.com>
2018-03-17 22:38:47 +0000

    adding install script for docker-ce

---
+++ install-docker-ce-ubuntu-1604.sh
@@ -0,0 +1,23 @@
+ #!/bin/bash
+
+ sudo apt-get update && \
+ sudo apt-get install -y \
+     apt-transport-https \
+     ca-certificates \
+     curl \
+     software-properties-common \
+     git
+
+ sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+ sudo apt-key fingerprint 0EBFCD88
+ sudo add-apt-repository \
+     "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+     $(lsb_release -cs) \
+     stable"
+
+ sudo apt-get update && \
+     sudo apt-get install -y docker-ce
+
+ sudo docker run hello-world
+
